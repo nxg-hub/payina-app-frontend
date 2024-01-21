27617dbd1f11518b7c7d4768c6b09c0b2c608cc0
encodedevv <timothy.onyeacholam@nxghubdigitech.onmicrosoft.com>
2024-01-21 02:10:31 +0100

    fix: form step

--- src/pages/Signup/signup-form.jsx
+++ src/pages/Signup/signup-form.jsx
@@ -40,7 +40,7 @@ export default function SignUpForm() {
 
   return (
     <div className="xl:mb-14 space-y-10 xl:w-fit w-full px-6 mx-auto absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-      {steps[9]}
+      {steps[currentStep]}
     </div>
   );
 }
