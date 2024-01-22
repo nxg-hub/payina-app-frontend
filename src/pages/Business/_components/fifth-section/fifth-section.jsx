08f0c7ce6004818c7336473f4c126337c6011216
Bigchills00 <chidera.eze@nxghubdigitech.onmicrosoft.com>
2024-01-22 21:36:58 +0100

    section 3,4,5&6 updated

--- src/pages/Business/_components/fifth-section/fifth-section.jsx
+++ src/pages/Business/_components/fifth-section/fifth-section.jsx
@@ -3,22 +3,18 @@ import section5 from './../../../../assets/images/section-four.png'
 
 const FifthSection = () => {
   return <div>
-    <div className='bg-lightBlue pt-16 grid md:grid-cols-2 items-center px-16'>
-      <div>
-        <img src={section5} alt="" />
-      </div>
-      <div className='flex flex-col  items-center'>
-        <p className='font-bold text-[48px] text-[white] '>One Login </p> 
-        <p className='font-bold text-[48px] text-[white] text-center '> Infinite Possibilities </p>
-        <button className='bg-[white] w-[200px] rounded-md font-medium my-6 mx-auto  py-3 text-[#000]'>Get Started</button>
-      </div>
-      
-    
+   <div className='bg-lightBlue pt-16 flex flex-col-reverse justify-between lg:flex-row items-center px-16'>
+  <div className=''>
+    <img src={section5} alt="" />
+  </div>
+  <div className='flex flex-col items-center'>
+    <p className='font-bold text-[36px] text-primary md:text-[48px] '>One Login </p>
+    <p className='font-bold text-[36px] text-primary text-center md:text-[48px] '>Infinite Possibilities </p>
+    <button className='bg-[white] w-[200px] rounded-md font-medium md:my-6 my-8 mx-auto py-3 text-[#000]'>Get Started</button>
+  </div>
+</div>
 
-
-    </div>
    </div>;
 };
 
 export default FifthSection;
- 
