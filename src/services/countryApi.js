214e8dd2021c26645e3c50fcbfd43315cfe3f002
encodedevv <timothy.onyeacholam@nxghubdigitech.onmicrosoft.com>
2024-01-25 09:32:43 +0100

    feature: country api, states and lg

---
+++ src/services/countryApi.js
@@ -0,0 +1,8 @@
+export const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '3a71d71b16msh192b8a1b2c7d629p109b5fjsn7416da672680',
+      'X-RapidAPI-Host': 'referential.p.rapidapi.com'
+    }
+}
+export const url = 'https://referential.p.rapidapi.com/v1/country?iso_a2=ng&limit=250';
