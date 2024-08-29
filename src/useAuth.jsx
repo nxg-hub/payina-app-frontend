7bd412957cb5b416c0aa0b6d0b11368e826a8319
Joseph Nma <josgolf@yahoo.ca>
2024-08-29 17:21:02 +0100

    reg level update.

--- src/useAuth.jsx
+++ src/useAuth.jsx
@@ -26,7 +26,7 @@ export function useAuth() {
                 navigate('/account/dashboard');
             }
         } else {
-            navigate('/');
+            navigate('/login');
         }
     }, [navigate]);
 }
