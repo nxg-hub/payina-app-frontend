9c556032cfbc68e36445e89be02d078bf168494a
Odili Chekwube Winifred <odilichekwubewinifred@gmail.com>
2024-10-04 08:20:06 +0100

    non-payina users integration

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import {
-  BASE_URL,
-  BASE_URL2, VITE_FETCH_BILLER_GROUPS,
+  BASE_URL2, VITE_FETCH_BILLER_DETAILS, VITE_FETCH_BILLER_GROUPS, VITE_FETCH_BILLER_PLANS,
   VITE_FETCH_DATA_PLANS,
   VITE_FETCH_SERVICES,
   VITE_INITIALIZE_PAYMENT,
@@ -106,104 +105,16 @@ export const apiService = {
   //   }
   // },
 
-//   fetchBillerDetails: async (billerGroupId) => {
-//     try {
-//       const response = await axios.get(`${VITE_FETCH_BILLER_DETAILS}/${billerGroupId}`);
-//       return response.data.data;
-//     } catch (error) {
-//       console.error('Error fetching biller details:', error);
-//       throw error;
-//     }
-//   }
+  fetchBillerPlans: async (billerSlug) => {
+    try {
+      const response = await axios.get(`${VITE_FETCH_BILLER_PLANS}/${billerSlug}`);
+      return response.data.data;
+    } catch (error) {
+      console.error('Error fetching biller details:', error);
+      throw error;
+    }
+  }
 };
 
 export default apiService;
 
-
-// import axios from 'axios';
-// import {
-//   VITE_FETCH_BILLER_GROUPS,
-//   VITE_FETCH_BILLER_DETAILS,
-//   VITE_FETCH_SERVICES,
-//   VITE_VEND_VALUE,
-//   VITE_CHECK_EMAIL_REGISTRATION,
-//   VITE_FETCH_NETWORKS,
-//   VITE_FETCH_DATA_PLANS,
-//   VITE_INITIALIZE_PAYMENT
-// } from '../utilities/Env';
-//
-// export const apiService = {
-//   checkEmailRegistration: async (email) => {
-//     const encodedEmail = encodeURIComponent(email);
-//     const response = await axios.get(`${VITE_CHECK_EMAIL_REGISTRATION}?email=${encodedEmail}`);
-//     return response.data;
-//   },
-//
-//   fetchNetworks: async () => {
-//     const response = await axios.get(`${VITE_FETCH_NETWORKS}`);
-//     return response.data.responseData;
-//   },
-//
-//   fetchDataPlans: async (networkSlug) => {
-//     const formattedUrl = networkSlug.replace(' ', '_');
-//     const response = await axios.get(`${VITE_FETCH_DATA_PLANS}/${formattedUrl}`);
-//     return response.data.responseData;
-//   },
-//
-//   initializePayment: async (planId, email, amount) => {
-//     const response = await axios.post(`${VITE_INITIALIZE_PAYMENT},`, {
-//       planId,
-//       email,
-//       amount
-//     });
-//     return response.data;
-//   },
-//
-//   vendValue: async (reference, payload) => {
-//     const response = await axios.post(`${VITE_VEND_VALUE}/${reference}`, payload);
-//     return response.data;
-//   },
-//
-//   fetchServices: async (serviceType) => {
-//     try {
-//       const response = await axios.get(`${VITE_FETCH_SERVICES}`);
-//       return response.data.responseData;
-//     } catch (error) {
-//       console.error(`Error fetching ${serviceType} services:`, error);
-//       throw error;
-//     }
-//   },
-//
-//   fetchBettingServices: async () => {
-//     try {
-//       const response = await fetch(`${VITE_FETCH_BILLER_GROUPS}`);
-//       if (!response.ok) {
-//         throw new Error('Failed to fetch betting services');
-//       }
-//       const data = await response.json();
-//
-//       // Check if data.responseData exists and is an array
-//       if (!data || !Array.isArray(data.responseData)) {
-//         throw new Error('Invalid response format: responseData not found or not an array');
-//       }
-//
-//       return data.responseData;
-//     } catch (error) {
-//       console.error('Error fetching betting services:', error);
-//       throw error;
-//     }
-//   },
-//
-//   fetchBillerDetails: async (billerGroupId) => {
-//     try {
-//       const response = await axios.get(`${VITE_FETCH_BILLER_DETAILS}/${billerGroupId}`);
-//       return response.data.data;
-//     } catch (error) {
-//       console.error('Error fetching biller details:', error);
-//       throw error;
-//     }
-//   }
-// };
-//
-// export default apiService;
-//
