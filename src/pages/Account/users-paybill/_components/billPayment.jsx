c59415b17592b7deb0e735b9d5d8f6327639ba92
ESTHER-OLA <estherolowo2018@gmail.com>
2024-10-28 00:31:07 +0100

    new updates

--- src/pages/Account/users-paybill/_components/billPayment.jsx
+++ src/pages/Account/users-paybill/_components/billPayment.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Phone, Wifi, Zap, Sliders } from 'lucide-react';
+// import { Phone, Wifi, Zap, Sliders } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 
 const BillOption = ({ icon, label, onClick }) => (
