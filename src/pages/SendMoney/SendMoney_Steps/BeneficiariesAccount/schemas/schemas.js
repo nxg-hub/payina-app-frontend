3636c2edde3443f39a2067b5a8c96db2ce9ec8e5
ESTHER-OLA <estherolowo2018@gmail.com>
2024-10-29 13:45:51 +0100

    sendMoney

--- src/pages/SendMoney/SendMoney_Steps/BeneficiariesAccount/schemas/schemas.js
+++ src/pages/SendMoney/SendMoney_Steps/BeneficiariesAccount/schemas/schemas.js
@@ -2,7 +2,6 @@ import * as Yup from 'yup';
 
 export const RecieverSchema = Yup.object().shape({
   beneficiaryName: Yup.string().required('Beneficiary Name is required'),
-  recieverName: Yup.string().required('Receiver Name Confirmation is required'),
 });
 
 export const AmountSchema = Yup.object().shape({
@@ -10,7 +9,7 @@ export const AmountSchema = Yup.object().shape({
   purpose: Yup.string().required('purpose is required'),
 });
 
-export const TransactionSchema = Yup.object().shape({
-  amount: Yup.string().required('Amount is required'),
-  purpose: Yup.string().required('purpose is required'),
-});
+// export const TransactionSchema = Yup.object().shape({
+//   amount: Yup.string().required('Amount is required'),
+//   purpose: Yup.string().required('purpose is required'),
+// });
